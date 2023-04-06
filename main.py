e530bd346c57393854db923062d0f284fabccd73
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-05 20:29:55 -0600

    Changed word from 'object' to 'objective' -HM3

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 ###
-#   The object of this script is to use it as a location to learn
+#   The objective of this script is to use it as a location to learn
 #   I will probably just stick each thing I learn into a separate file and just include it into here
 #   Tomorrow I will work on updating this to work with anything I add to it
 #   The ideal layout, would grab everything in this folder, list it all, allowing the user (myself) to choose one and run it
 #   Should be easy enough, and well with-in my abilities.
 #   After I finish that, I would like to add the ability to save input data, whatever we do to it, and output data with an easy comparison.
 ###
-
 
 from time import sleep #import sleep for delay at end of program
 print("Hello World") #Say hello world to terminal
