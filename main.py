78f0be19613fcde4b1e0d97fa73b7d7f76407d28
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-05 20:27:45 -0600

    Creating repo, and making main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+###
+#   The object of this script is to use it as a location to learn
+#   I will probably just stick each thing I learn into a separate file and just include it into here
+#   Tomorrow I will work on updating this to work with anything I add to it
+#   The ideal layout, would grab everything in this folder, list it all, allowing the user (myself) to choose one and run it
+#   Should be easy enough, and well with-in my abilities.
+#   After I finish that, I would like to add the ability to save input data, whatever we do to it, and output data with an easy comparison.
+###
+
+
+from time import sleep #import sleep for delay at end of program
+print("Hello World") #Say hello world to terminal
+sleep(2) #This way user(Me) can see output no matter how they(I) run it
+
+
+
+
+for i in range(20): #Clear screen when done, we don't want any ugly clutter
+    print()
+      
