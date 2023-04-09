06525c2663158ed521fac5ebf3475e362b39f7cb
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-08 21:21:53 -0600

    Updated TODO

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 ###
+# TODO:
 #   The objective of this script is to use it as a location to learn
 #   I will probably just stick each thing I learn into a separate file and just include it into here
 #   Tomorrow I will work on updating this to work with anything I add to it
 #   The ideal layout, would grab everything in this folder, list it all, allowing the user (myself) to choose one and run it
+#       Everything Will actually be in this same dir because other wise it gets touchy
 #   Should be easy enough, and well with-in my abilities.
 #   After I finish that, I would like to add the ability to save input data, whatever we do to it, and output data with an easy comparison.
 ###
