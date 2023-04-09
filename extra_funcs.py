eb03325465a10a756d0ca07a79ce165f948701dd
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-09 14:53:31 -0600

    Moved error catching over to extra funcs to keep main file simple

--- extra_funcs.py
+++ extra_funcs.py
@@ -1,3 +1,5 @@
+from time import sleep
+import importlib
 def clean_output():
     for i in range(20): # Clear screen when done, we don't want any ugly clutter
         print()
@@ -16,3 +18,32 @@ def get_files():
             continue
         fixed_names.append(name[0:-3])
     return fixed_names
+
+def run_file(fixed_names, user_input):
+    if user_input[0].lower() == 'q':
+        quit = True
+    else:
+        quit = False
+    try:
+        if quit:
+            raise IndexError
+        cs = importlib.import_module(fixed_names[int(user_input)]) #cs = custom file
+        cs.main()
+        pause()
+        print("\n\n\n")
+        return True
+    except IndexError:
+        print("\n\n\n")
+        print("GoodBye!")
+        return False
+    except ValueError:
+        print("\n\n\n")
+        print("Not valid input! Try again!")
+        sleep(1)
+        return True
+    except Exception as inst:
+        print("----ERROR----")
+        print(type(inst))
+        print(inst)
+        print("----ERROR----")
+        return False
