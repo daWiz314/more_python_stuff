761a0bf826e7a1fb9b967f667e329c2941c27ae7
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-09 14:39:07 -0600

    Extra functions to clean up main file

---
+++ extra_funcs.py
@@ -0,0 +1,8 @@
+def clean_output():
+    for i in range(20): # Clear screen when done, we don't want any ugly clutter
+        print()
+
+def pause():
+    print("Press enter to continue")
+    input()
+    return
