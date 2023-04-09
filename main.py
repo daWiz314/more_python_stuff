7ec8261c6b49d57207333ffa9752a82a95282381
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-08 21:41:16 -0600

    Better formatting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #   I will probably just stick each thing I learn into a separate file and just include it into here
 #   Tomorrow I will work on updating this to work with anything I add to it
 #   The ideal layout, would grab everything in this folder, list it all, allowing the user (myself) to choose one and run it
-#       Everything Will actually be in this same dir because other wise it gets touchy
+#       ---Everything Will actually be in this same dir because other wise it gets touchy
 #   Should be easy enough, and well with-in my abilities.
 #   After I finish that, I would like to add the ability to save input data, whatever we do to it, and output data with an easy comparison.
 ###
@@ -42,6 +42,7 @@ while True:
         cs = importlib.import_module(fixed_names[int(user_input)]) #cs = custom file
         cs.main()
         sleep(2)
+        print("\n\n\n")
     except IndexError:
         print("\n\n\n")
         print("GoodBye!")
