174f3b7a1e13950337444fd38f9f3e56b94c2288
Harry <50802261+daWiz314@users.noreply.github.com>
2023-04-09 22:13:52 -0600

    Just uploading

--- gui.py
+++ gui.py
@@ -8,10 +8,8 @@ class App(tk.Tk):
         self.geometry('400x300')
         self.resizable(0,0)
 
-        self.rowconfigure(0, weight=0)
-        self.rowconfigure(1, weight=0)
         self.columnconfigure(0, weight=0)
-        self.columnconfigure(1, weight=0)
+        
 
         self.__create_widgets(content)
         del self.content
@@ -21,9 +19,9 @@ class App(tk.Tk):
         scroll_frame = tk.Frame(self)
         button_frame = tk.Frame(self)
 
-        LabelFrame(welcome_message).grid(column=0, row=0, sticky=tk.NE) # Welcome Message
-        ListBoxFrame(scroll_frame, content).grid(column=0, row=1) # List Box and Scroll Bar
-        ButtonFrame(button_frame).grid(column=3, row=1, sticky=tk.E)
+        LabelFrame(self).grid(column=1, row=0, sticky=tk.NE) # Welcome Message
+        ListBoxFrame(self, content).grid(column=0, row=1) # List Box and Scroll Bar
+        ButtonFrame(self).grid(column=2, row=1, sticky=tk.E)
         # welcome_message.grid(column=2, row=0, sticky=tk.NSEW) # Display the frame for the welcome message
         # scroll_frame.grid(column=0, row=1) # Display the frame for the scroll bar
         for child in self.winfo_children():
@@ -35,7 +33,7 @@ class LabelFrame(tk.Frame):
         self.__create_widgets()
     
     def __create_widgets(self):
-        self.main_label = tk.Label(self, text="Welcome! Please select an option.", width=30).grid(column=1, row=0, sticky=tk.N)
+        self.main_label = tk.Label(self, text="Welcome! Please select an option.", width=30).grid(column=0, row=0, sticky=tk.NSEW)
 
 class ButtonFrame(tk.Frame):
     def __init__(self, container):
@@ -54,12 +52,12 @@ class ListBoxFrame(tk.Frame):
     
     def __create_widgets(self, container):
         self.sb = tk.Scrollbar(container, orient=tk.VERTICAL) # sb = Scroll Bar
-        self.lb = tk.Listbox(container, height=10, width=15, listvariable=self.content, yscrollcommand=self.sb.set).grid(row=0, column=0) # lb = List Box
+        self.lb = tk.Listbox(container, height=10, width=15, listvariable=self.content, yscrollcommand=self.sb.set).grid(row=1, column=0) # lb = List Box
 
-        self.sb.grid(row=0, column=1, sticky=tk.NS)
+        self.sb.grid(row=1, column=1, sticky=tk.NS)
 
-        for child in self.winfo_children():
-            child.grid(padx=0, pady=5)
+        # for child in self.winfo_children():
+        #     child.grid(padx=0, pady=5)
 
 
 app = App()
